e4cfe005220ac857fb4b05a5302d0204a96eda67
Rachel Sklar <rsklar@berkeley.edu>
2016-03-26 17:46:36 +0200

    march 26

--- Gitega.R
+++ Gitega.R
@@ -23,52 +23,78 @@ countsdf<-as.data.frame(git_counts)
 #prices of different emptying practices
 #average price of sealing a latrine then building a new one yourself 
 
-#respondents that chose empty practice 1
-git_emptypract1<-subset(gitresult,gitresult$emptypract==1)
-git_costpract1<-git_emptypract1$cost_empty[!git_emptypract1$cost_empty==99]
-gitmeancost1<-mean(git_costpract1)
+# definition of function
+costofpract<-function(result,practice){
+  emptypract<-subset(result,result$emptypract==practice)
+  cost<-emptypract$cost_empty[!emptypract$cost_empty==99]
+  meancost<-mean(cost,na.rm=TRUE)
+  return(meancost)
+}
 
-git_emptypract5<-subset(gitresult,gitresult$emptypract==5)
-git_costpract5<-git_emptypract5$cost_empty[!git_emptypract5$cost_empty==99]
-gitmeancost5<-mean(git_costpract5)
+#usage of function
+gitmeancost1 <- costofpract(gitresult, 1)
+gitmeancost2 <- costofpract(gitresult, 2)
+gitmeancost3 <- costofpract(gitresult, 3)
+gitmeancost4 <- costofpract(gitresult, 4)
+gitmeancost5 <- costofpract(gitresult, 5)
+gitmeancost6 <- costofpract(gitresult, 6)
+gitmeancost7 <- costofpract(gitresult, 7)
+gitmeancost8 <- costofpract(gitresult, 8)
+gitmeancost9 <- costofpract(gitresult, 9)
+gitmeancost10 <- costofpract(gitresult, 10)
+gitmeancost11 <- costofpract(gitresult, 11)
+gitmeancost12 <- costofpract(gitresult, 12)
+gitmeancost13 <- costofpract(gitresult, 13)
+gitmeancost14 <- costofpract(gitresult, 14)
+gitmeancost15 <- costofpract(gitresult, 15)
+gitmeancost16 <- costofpract(gitresult, 16)
+gitmeancost17 <- costofpract(gitresult, 77)
+gitmeancost18 <- costofpract(gitresult, 99)
 
-git_emptypract7<-subset(gitresult,gitresult$emptypract==7)
-git_costpract7<-git_emptypract7$cost_empty[!git_emptypract7$cost_empty==99]
-gitmeancost7<-mean(git_costpract7)
+gitmeancost_vect<-c(gitmeancost1,
+                    gitmeancost2,
+                    gitmeancost3,
+                    gitmeancost4,
+                    gitmeancost5,
+                    gitmeancost6,
+                    gitmeancost7,
+                    gitmeancost8,
+                    gitmeancost9,
+                    gitmeancost10,
+                    gitmeancost11,
+                    gitmeancost12,
+                    gitmeancost13,
+                    gitmeancost14, 
+                    gitmeancost15,
+                    gitmeancost16,
+                    gitmeancost17,
+                    gitmeancost18)
 
-git_emptypract9<-subset(gitresult,gitresult$emptypract==9)
-git_costpract9<-git_emptypract9$cost_empty[!git_emptypract9$cost_empty==99]
-gitmeancost9<-mean(git_costpract9)
+gitmeancost_labs<-c("gitmeancost1",
+                    "gitmeancost2",
+                    "gitmeancost3",
+                    "gitmeancost4",
+                    "gitmeancost5",
+                    "gitmeancost6",
+                    "gitmeancost7",
+                    "gitmeancost8",
+                    "gitmeancost9",
+                    "gitmeancost10",
+                    "gitmeancost11",
+                    "gitmeancost12",
+                    "gitmeancost13",
+                    "gitmeancost14", 
+                    "gitmeancost15",
+                    "gitmeancost16",
+                    "gitmeancost17",
+                    "gitmeancost18")
+
+as.data.frame(cbind(gitmeancost_labs,gitmeancost_vect))
 
 
-git_emptypract13<-subset(gitresult,gitresult$emptypract==13)
-git_costpract13<-git_emptypract13$cost_empty[!git_emptypract13$cost_empty==99]
-gitmeancost13<-mean(git_costpract13)
+gitmeancost_vect[!gitmeancost_vect=="NaN"]
+barplot(gitmeancost_vect,xlab="Emptying Practice",ylab="Price RWF")
 
-#14 removed from list of means to display in graph because the only values are 99
-git_emptypract14<-subset(gitresult,gitresult$emptypract==14)
-git_costpract14<-git_emptypract14$cost_empty[!git_emptypract14$cost_empty==99]
-gitmeancost14<-mean(git_costpract14,na.rm=TRUE)
 
-git_emptypract16<-subset(gitresult,gitresult$emptypract==16)
-git_costpract16<-git_emptypract16$cost_empty[!git_emptypract16$cost_empty==99]
-gitmeancost16<-mean(git_costpract16)
-
-git_emptypract99<-subset(gitresult,gitresult$emptypract==99)
-git_costpract99<-git_emptypract99$cost_empty[!git_emptypract99$cost_empty==99]
-gitmeancost99<-mean(git_costpract99)
-
-#labelmeancosts<-c("1","2","3","5","7","8","9","10","11","14")
-labelmeancosts<-c("1","5","9","13","16")
-#git_meancostempty<-c(gitmeancost1,gitmeancost5,gitmeancost7,gitmeancost9,gitmeancost13,gitmeancost14,gitmeancost16)
-git_meancostempty<-c(gitmeancost1,gitmeancost5,gitmeancost9,gitmeancost13,gitmeancost16)
-
-barplot(git_meancostempty,names.arg=c("1","5","9","13","16"),xlab="Emptying Practice",ylab="Price RWF")
-
-#things to work out:
-  #git_emptypract7$costempty[!git_emptypract7$cost_empty==99]....why isn't it removing the record==99?
-  #they didn't know the cost of empty 7 so don't include it in the price summary
-
-#also had to take out 14 because gitmeancost14=NA
-#the only values for 14 were 99, so the mean ended up being Nan 
-#how can i take out categories whose mean endsup being NaN? 
+#when I run the code below,  means are generated for practices that have no records:
+#as.data.frame(cbind(gitmeancost_labs,gitmeancost_vect[!gitmeancost_vect=="NaN"]))
